6eb1d474f7931eb0e4dc562d0a57f02c40a7acf2
Mohammedsalman14 <mohamedsalman140@gmail.com>
2023-04-13 18:02:22 +0530

    Using inbuild function `assert` to check the validation in Main.class.

--- Main.py
+++ Main.py
@@ -1,21 +1,29 @@
 
 
 class Item:
-    def __init__(self, name,price,quantity):
-    
-        self.name=name
-        self.price=price
-        self.quantity=quantity
+    def __init__(self, name: str, price: float, quantity: float):
+        # Checking validation to the Received Arguments
 
-    def calculate_total_price(self, x, y):
-        return x*y
+        assert price >=0
+        assert quantity >=0
+
+        # Assigning parms to self
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def calculate_total_price(self):
+        return self.price*self.quantity
 
 
-item1 = Item("Phone",100,5)
+item1 = Item("Phone", 100, 5)
 
-print(item1.name,item1.price,item1.quantity)
 
-item2 = Item("Laptop",1000,3)
+print("Item1: ", item1.calculate_total_price())
 
-print(item2.name,item2.price,item2.quantity)
+item2 = Item("Laptop", 1000, 3)
 
+print("Item2: ", item2.calculate_total_price())
+
+
+# print(2*"salman")
