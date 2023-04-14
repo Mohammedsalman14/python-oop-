1e4663b45a44f2d9aee281aeaef7c888f15c4aa3
Mohammedsalman14 <mohamedsalman140@gmail.com>
2023-04-14 18:02:53 +0530

    appending the all `instance`

--- Main.py
+++ Main.py
@@ -2,33 +2,33 @@
 
 class Item:
 
-    pay_rate=0.8
+    pay_rate = 0.8
+    all = []
 
     def __init__(self, name: str, price: float, quantity: float):
         # Checking validation to the Received Arguments
 
-        assert price >=0,f"price {price} is not greater than zero"
-        assert quantity >=0,f"qantity {quantity} is not greater than zero"
+        assert price >= 0, f"price {price} is not greater than zero"
+        assert quantity >= 0, f"qantity {quantity} is not greater than zero"
 
         # Assigning parms to self
         self.name = name
         self.price = price
         self.quantity = quantity
 
+        self.all.append(self)
+
     def calculate_total_price(self):
         return self.price*self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
 
 item1 = Item("Phone", 100, 5)
-
-
-print("Item1: ", item1.calculate_total_price())
-
 item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 1000, 1)
+item4 = Item('Charger', 100, 2)
 
-print("Item2: ", item2.calculate_total_price())
-
-print(Item.__dict__) #All the attributes for Class level
-print(item1.__dict__) #All the attributes for Instance level
-
-# print(2*"salman")
+for instance in Item.all:
+    print("Instance: ", instance.name)
