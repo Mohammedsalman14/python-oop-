05cddec9c2b1bdee86b4b09aba10eb8aa393d15f
Mohammedsalman14 <mohamedsalman140@gmail.com>
2023-04-18 11:21:33 +0530

    Starting with Inheritance

--- Main.py
+++ Main.py
@@ -1,10 +1,43 @@
-import csv342
+import csv
+
 
 class Item:
 
     pay_rate = 0.8
     all = []
 
+    def calculate_total_price(self):
+        return self.price*self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self) -> str:
+        return f"Items('{self.name}',{self.price},{self.quantity})"
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('item.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get(
+                    'price')),
+                quantity=int(item.get('quantity')))
+
+    @staticmethod
+    def is_integer(num):
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
+
+class Phone(Item):
     def __init__(self, name: str, price: float, quantity: float):
         # Checking validation to the Received Arguments
 
@@ -18,19 +51,6 @@ class Item:
 
         self.all.append(self)
 
-    def calculate_total_price(self):
-        return self.price*self.quantity
 
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
-    def __repr__(self) -> str:
-        return f"Items('{self.name}',{self.price},{self.quantity})"
-
-item1 = Item("Phone", 100, 5)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 1000, 1)
-item4 = Item("Charger", 100, 2)
-
-print(Item.all)
-
-
+phone1 = Phone("apple", 500, 5)
+print(phone1.all)
